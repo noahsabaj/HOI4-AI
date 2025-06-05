faedba71ac598c9c5b29f43a7537d9ddb0e85118
Noah Sabaj <noahsabaj@gmail.com>
2025-06-05 12:10:23 -0400

    adjustments to learning and init

--- src/ai/__init__.py
+++ src/ai/__init__.py
@@ -1,6 +1,4 @@
 # src/ai/__init__.py
-from .brain import HOI4Brain
 from .memory import StrategicMemory, GameMemory
-from .learner import UnifiedHOI4Learner
 
-__all__ = ['HOI4Brain', 'StrategicMemory', 'GameMemory', 'UnifiedHOI4Learner']
+__all__ = ['StrategicMemory', 'GameMemory']
