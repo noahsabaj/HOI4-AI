121efc35d23e99e6240b8a33fb6e4be7af1c89a8
noahsabaj <noahsabaj@gmail.com>
2026-04-17 13:23:49 -0400

    major rework

--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-# src/__init__.py
-"""HOI4 AI - Self-learning AI for Hearts of Iron 4"""
-__version__ = "1.0.0"
--- src/ai/__init__.py
+++
@@ -1,4 +0,0 @@
-# src/ai/__init__.py
-from .memory import StrategicMemory, GameMemory
-
-__all__ = ['StrategicMemory', 'GameMemory']
